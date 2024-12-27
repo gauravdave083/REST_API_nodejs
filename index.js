a6a92867914b52f6a7feb38a0137356383695ed8
gauravdave083 <gd836398@gmail.com>
2024-12-27 10:25:17 +0530

    updates

--- index.js
+++ index.js
@@ -22,12 +22,35 @@ app.get('/api/users', (req, res) => {
      return res.json(users)
 })
 
-app.get("/api/users/:id", (req, res) => {
-     const id = Number(req.params.id);
-     const user = users.find(user => user.id === id)
-     return res.json(user);
-})
+
+app
+     .route('/api/users/:id')
+     .get((req, res) => {
+          const id = Number(req.params.id);
+          const user = users.find(user => user.id === id)
+          return res.json(user);
+     })
+     .put((req, res) => {
+          return res.json({ status: "Pending" })
+     })
+     .delete((req, res) => {
+          return res.json({ status: "Pending" })
+     })
 
 
+// app.post("/api/users", (req, res) => {
+//      // TODO : Create new user
+//      return res.json({ status: "pending"})
+// })
+
+// app.patch("/api/users/:id", (req, res) => {
+//      // TODO : Edit the user with Id
+//      return res.json({ status: "pending"})
+// })
+
+// app.delete("/api/users/:id", (req, res) => {
+//      // TODO : delete the user with Id
+//      return res.json({ status: "pending"})
+// })
 
 app.listen(PORT, () => console.log/(`Server Started at PORT: ${PORT}`))
