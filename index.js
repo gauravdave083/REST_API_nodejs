3368c4445b0ecc6d54d3e78a531d7e75638ab97b
gauravdave083 <gd836398@gmail.com>
2024-12-28 15:37:47 +0530

    updates

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ app
 
 app.post("/api/users", (req, res) => {
      const body = req.body;
-     users.push({...body, id: users.length + 1});
+     users.push({...body, id: users.length +      1});
      fs.writeFile('./MOCK_DATA,json', JSON.stringify(users), (err, data) => {
           return res.json({ status: "pending"})
      })
